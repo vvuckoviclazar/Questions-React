104584d6d39962484d4127d84ddc82362ff961bc
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-06-24 08:36:05 +0200

    Finished

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import "./index.css";
 
 function App() {
   const [questions] = useState(questionsData);
+  const [openInfo, setOpenInfo] = useState({});
+
+  const toggleInfo = (id) => {
+    setOpenInfo((prev) => ({ ...prev, [id]: !prev[id] }));
+  };
 
   return (
     <section className="list">
@@ -13,8 +18,36 @@ function App() {
           <li key={question.id}>
             <div className="question-div">
               <h4>{question.title}</h4>
-              <button className="info-btn">+</button>
+              <button
+                className="info-btn"
+                onClick={() => toggleInfo(question.id)}
+              >
+                {openInfo[question.id] ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4 10a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H4.75A.75.75 0 0 1 4 10Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+                  </svg>
+                )}
+              </button>
             </div>
+            {openInfo[question.id] && <p>{question.info}</p>}
           </li>
         ))}
       </ul>
