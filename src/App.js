32a10cc3b2984303a6f9c2c468beb7e3cda8d82a
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-06-27 06:30:17 +0200

    Finished...

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
 import { useState } from "react";
 import questionsData from "./data";
 import "./index.css";
-import SingleQuestion from "./SingleQuestion";
 
 function App() {
-  const [questions] = useState(questionsData);
+  const [questions, setQuestions] = useState(questionsData);
+
+  const toggleQuestion = (id) => {
+    const updated = questions.map((data) =>
+      data.id === id ? { ...data, isExpanded: !data.isExpanded } : data
+    );
+    setQuestions(updated);
+  };
 
   return (
     <section className="list">
       <h1>Questions</h1>
       <ul className="questions-list">
         {questions.map((question) => (
-          <SingleQuestion key={question.id} question={question} />
+          <li key={question.id}>
+            <div className="question-div">
+              <h4>{question.title}</h4>
+              <button
+                className="info-btn"
+                onClick={() => toggleQuestion(question.id)}
+              >
+                {question.isExpanded ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4 10a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H4.75A.75.75 0 0 1 4 10Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+                  </svg>
+                )}
+              </button>
+            </div>
+            {question.isExpanded && <p>{question.info}</p>}
+          </li>
         ))}
       </ul>
     </section>
--- src/data.js
+++ src/data.js
@@ -3,26 +3,31 @@ const questions = [
     id: 1,
     title: "Do I have to allow the use of cookes?",
     info: "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
+    isExpanded: false,
   },
   {
     id: 2,
     title: "How do I change my My Page password?",
     info: "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
+    isExpanded: false,
   },
   {
     id: 3,
     title: "What is BankID?",
     info: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+    isExpanded: false,
   },
   {
     id: 4,
     title: "Whose birth number can I use?",
     info: "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
+    isExpanded: false,
   },
   {
     id: 5,
     title: "When do I recieve a password ordered by letter?",
     info: "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+    isExpanded: false,
   },
 ];
 export default questions;
